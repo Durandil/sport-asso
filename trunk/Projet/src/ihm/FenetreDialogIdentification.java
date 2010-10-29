17bf89942d287cb464eedeffb86ec83071a0711d
remboss_6@hotmail.fr <remboss_6@hotmail.fr@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-10-29 18:23:53 +0000

    Ajout de fenetres de message d'erreur pour FenetreDialogIdentifcation

--- trunk/Projet/src/ihm/FenetreDialogIdentification.java
+++ trunk/Projet/src/ihm/FenetreDialogIdentification.java
@@ -9,10 +9,12 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -25,6 +27,7 @@ public class FenetreDialogIdentification extends JDialog {
 	DialogIdentifiant ident= new DialogIdentifiant();
 	private JLabel identifiantLabel,passwordLabel;
 	private JTextField identifiant, password;
+	private JOptionPane erreurMotPasse, erreurCompte;
 	
 	
 	public FenetreDialogIdentification(JFrame parent, String title, boolean modal){
@@ -82,6 +85,7 @@ public class FenetreDialogIdentification extends JDialog {
 		JButton validationBouton = new JButton("Valider");
 		
 		validationBouton.addActionListener(new ActionListener(){
+			@SuppressWarnings("static-access")
 			public void actionPerformed(ActionEvent arg0) {
 				int present = 0;
 				ArrayList<String[]> listeMailsMdps = new ArrayList<String[]>();
@@ -96,9 +100,13 @@ public class FenetreDialogIdentification extends JDialog {
 						if(password.getText().equals(listeMailsMdps.get(i)[1])){
 							System.out.println("Identification réussie !");
 						//Faire apparaître le Menu utilisateur ici !
+						MenuUtilisateur men = new MenuUtilisateur();
 						}
 						else{
-							System.out.println("Mot de passe erroné, veuillez réessayer.");
+							// essai d'affichage d'un message d'erreur en cas de mot de passe erroné
+							erreurMotPasse = new JOptionPane();
+							ImageIcon image = new ImageIcon("src/images/warning.png");
+							erreurMotPasse.showMessageDialog(null, "Mot de passe erroné, veuillez réessayer.", "Attention", JOptionPane.WARNING_MESSAGE, image);
 						}
 					}
 					
@@ -106,7 +114,10 @@ public class FenetreDialogIdentification extends JDialog {
 				
 				if(present == 0)
 				{
-					System.out.println("Ce compte n'existe pas, inscrivez-vous !");
+					// essai d'affichage d'un message d'erreur en cas de probleme sur le compte
+					erreurCompte = new JOptionPane();
+					ImageIcon image = new ImageIcon("src/images/warning.png");
+					erreurCompte.showMessageDialog(null, "Ce compte n'existe pas, inscrivez-vous !", "Attention", JOptionPane.WARNING_MESSAGE, image);
 				}
 				// vérifier que le login existe sinon retourner à la page précédente de choix entre création et identification
 				// lancer la page suivante si succès
