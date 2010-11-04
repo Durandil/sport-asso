afd481d20d91ae1e621a5cb96abac6780c897332
jul.neveu@laposte.net <jul.neveu@laposte.net@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-11-04 08:27:53 +0000

    j'ai rien fait c'etait juste pour voir

--- trunk/Projet/src/ihm/ClassePrincipale.java
+++ trunk/Projet/src/ihm/ClassePrincipale.java
@@ -12,9 +12,9 @@ public class ClassePrincipale {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//BDD.creerTable("CLIENTS");
-		//FenetreCompte fen=new FenetreCompte();
+		FenetreCompte fen=new FenetreCompte();
 		//MenuUtilisateur men = new MenuUtilisateur();
-		MenuGerant menu= new MenuGerant();
+		//MenuGerant menu= new MenuGerant();
 		
 	}
 
