831ccee8d628b249515b269495def39f236c8a95
Nguyen.Sullivan <Nguyen.Sullivan@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-11-05 16:44:25 +0000

    Création d'une classe Promotion (avec la méthode ajouterBDD) Début de remodélisation de la classe Catégorie

--- trunk/Projet/src/metier/Categorie.java
+++ trunk/Projet/src/metier/Categorie.java
@@ -8,55 +8,11 @@ import basededonnees.SGBD;
 public class Categorie {
 
 	// Constructeur d'une catégorie d'articles
-	// Celle-ci se définit par un identifiant et une liste de couples (quantité/réduction)
-	// La réduction est exprimée à travers un coefficient multiplicateur inférieur à 1
-	public Categorie(String identifiant,
-			ArrayList<double[]> couplePromo) {
-		this.identifiant = identifiant;
-		this.couplePromo = couplePromo;
-	}
+	// Celle-ci se définit par un id, un nom et une quantité limite
+	// Lorsque cette quantité limite est atteinte, le gérant est averti d'un besoin de réapprovisionnement
 	
-	String identifiant;
-	ArrayList<double[]> couplePromo;
-	
-	public String getIdentifiant() {
-		return identifiant;
-	}
-	public void setIdentifiant(String identifiant) {
-		this.identifiant = identifiant;
-	}
-	
-	public ArrayList<double[]> getCouplePromo() {
-		return couplePromo;
-	}
-	public void setCouplePromo(ArrayList<double[]> couplePromo) {
-		this.couplePromo = couplePromo;
-	}
-
-
-	// Méthode permettant de créer une table :
-	// Chaque Catégorie à une table qui lui est associée
-	
-	// Au moment de l'insertion, on ajoute le 1er élément du couple i puis le second
-	public void creerTable(){
-		SGBD.executeUpdate("DROP TABLE " + this.identifiant);
-		SGBD.executeUpdate("CREATE TABLE " + this.identifiant
-				+ " (QUANTITE NUMBER(3),"
-				+ " COEFFICIENT NUMBER(3,2) )"
-				);
-		for(int i=0; i <this.couplePromo.size();i++)
-		{
-
-			String requete = "INSERT INTO " + this.identifiant+ " (QUANTITE, COEFFICIENT) VALUES ( "
-				
-				+ this.couplePromo.get(i)[0]
-				+ ","
-				+ this.couplePromo.get(i)[1]
-				+ ") ";
-		
-		SGBD.executeUpdate(requete);
-		}
-		
-	}
+	private String idCategorie;
+	private String nomCategorie;
+	private int quantiteLimite;
 	
 }
---
+++ trunk/Projet/src/metier/Promotion.java
@@ -0,0 +1,80 @@
+package metier;
+
+import java.sql.Date;
+
+import basededonnees.SGBD;
+
+public class Promotion {
+
+	public Promotion(String idPromotion, String nomPromotion, Date dateDebut,
+			Date dateFin, double pourcentagePromo) {
+		super();
+		this.idPromotion = idPromotion;
+		this.nomPromotion = nomPromotion;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.pourcentagePromo = pourcentagePromo;
+		ajouterBDD();
+	}
+	
+	private String idPromotion;
+	private String nomPromotion;
+	private Date dateDebut;
+	private Date dateFin;
+	private double pourcentagePromo;
+	
+	public String getIdPromotion() {
+		return idPromotion;
+	}
+	public void setIdPromotion(String idPromotion) {
+		this.idPromotion = idPromotion;
+	}
+	public String getNomPromotion() {
+		return nomPromotion;
+	}
+	public void setNomPromotion(String nomPromotion) {
+		this.nomPromotion = nomPromotion;
+	}
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public double getPourcentagePromo() {
+		return pourcentagePromo;
+	}
+	public void setPourcentagePromo(float pourcentagePromo) {
+		this.pourcentagePromo = pourcentagePromo;
+	}
+	
+	public void ajouterBDD() {
+		
+		String s = SGBD.transformation(this.dateDebut);
+		String s2 = SGBD.transformation(this.dateFin);
+		
+		
+		String requete = "INSERT INTO PROMO (IDPROMO, NOMPROMO, DATEDEBUT," +
+		" DATEFIN, POURCENTAGEPROMO) VALUES ( "
+		+ "'"
+		+ this.idPromotion
+		+ "',"
+		+ "'"
+		+ this.nomPromotion
+		+ "',"
+		+ s 
+		+ "," 
+		+ s2
+		+ "," 
+		+ this.pourcentagePromo
+		+")";
+		
+		SGBD.executeUpdate(requete);
+	}
+}
