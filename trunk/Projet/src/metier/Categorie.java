a6d8be0c9a397dc521323bbdb0e046110787d2d4
Nguyen.Sullivan <Nguyen.Sullivan@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-11-07 17:37:28 +0000

    Ajout du fichier Insert.txt (légèrement modifié) dans le paquetage basededonnees
    Classe Categorie opérationnelle

--- trunk/Projet/src/metier/Categorie.java
+++ trunk/Projet/src/metier/Categorie.java
@@ -11,8 +11,48 @@ public class Categorie {
 	// Celle-ci se définit par un id, un nom et une quantité limite
 	// Lorsque cette quantité limite est atteinte, le gérant est averti d'un besoin de réapprovisionnement
 	
+	public Categorie(String idCategorie, String nomCategorie, int quantiteLimite) {
+		this.idCategorie = idCategorie;
+		this.nomCategorie = nomCategorie;
+		this.quantiteLimite = quantiteLimite;
+		ajouterBDD();
+	}
+	private void ajouterBDD() {
+		// TODO Auto-generated method stub
+	
+	
+		String requete = "INSERT INTO CATEGORIE (IDCATEGORIE, NOMCATEGORIE, QUANTITELIMITE) VALUES ( "
+		+ "'"
+		+ this.idCategorie
+		+ "',"
+		+ "'"
+		+ this.nomCategorie
+		+ "',"
+		+ this.quantiteLimite
+		+")";
+
+		SGBD.executeUpdate(requete);
+	}
 	private String idCategorie;
 	private String nomCategorie;
 	private int quantiteLimite;
+	public void setQuantiteLimite(int quantiteLimite) {
+		this.quantiteLimite = quantiteLimite;
+	}
+	public int getQuantiteLimite() {
+		return quantiteLimite;
+	}
+	public void setNomCategorie(String nomCategorie) {
+		this.nomCategorie = nomCategorie;
+	}
+	public String getNomCategorie() {
+		return nomCategorie;
+	}
+	public void setIdCategorie(String idCategorie) {
+		this.idCategorie = idCategorie;
+	}
+	public String getIdCategorie() {
+		return idCategorie;
+	}
 	
 }
