f14049a7926c2be326379649cc272af3bee905ab
jul.neveu@laposte.net <jul.neveu@laposte.net@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-11-25 09:28:25 +0000

    test sur la sélection de dates dans la BDD valide

--- trunk/Projet/src/test/SGBDTest.java
+++ trunk/Projet/src/test/SGBDTest.java
@@ -2,27 +2,38 @@ package test;
 
 import java.util.ArrayList;
 
+import metier.Promotion;
+
 import application.Principale;
 import basededonnees.SGBD;
 
 import junit.framework.TestCase;
 
 public class SGBDTest extends TestCase {
-	public void testSelectListeString(){
+	
+// test select liste opérationnel	
+
+	/*	public void testSelectListeString(){
 		ArrayList<String> result= new ArrayList<String>();
 		result.add("0256842210");
 		result.add("0256986633");
 		result.add("0299451210");
 		
 		assertEquals(result,SGBD.selectListeString("client","TELEPHONE"));
-	}
-//	public void testSelectListeDates(){
-//		ArrayList<String> result = new ArrayList<String>();
-//		String dateS = "05/11/2010";
-//		String dateS2 = "07/11/2010";
-//		result.add(dateS);
-//		result.add(dateS2);
-//		assertEquals(result,SGBD.selectListeDates(table, str, format) 
-//	}
+	}*/
+	
+// test select date pour date fin (l'ordre doit être le même dans la base et dans le résultat)	
+
+	/*	public void testSelectListeDates(){
+		ArrayList<String> result = new ArrayList<String>();
+		String dateS = "07/11/10";
+		String dateS2 = "30/11/10";
+		result.add(dateS);
+		result.add(dateS2);
+		assertEquals(result,SGBD.selectListeDates("Promo","datefin" ,"dd/mm/yy")); 
+	}*/
+	
+	
 }
+
 
