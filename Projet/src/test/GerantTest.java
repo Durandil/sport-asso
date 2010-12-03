1455b425480bc9f9d17d54a36a39fd4025ace31d
jul.neveu@laposte.net <jul.neveu@laposte.net@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-12-03 16:07:02 +0000

    nouvelles classes tests

---
+++ Projet/src/test/GerantTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import basededonnees.SGBD;
+import metier.Gerant;
+import junit.framework.TestCase;
+
+public class GerantTest extends TestCase {
+	
+	public void testActivDesactivCompte(){
+		// Désactiver le compte de Laroch Arthur
+		Gerant.activDesactivCompte();
+		
+		String result="Désactivé";
+		assertEquals(result,SGBD.selectStringConditionString("Client","EtatCompte" , "NomClient", "Laroch") );
+	}
+}
