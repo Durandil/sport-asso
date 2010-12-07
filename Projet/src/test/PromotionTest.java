83c45b8be718ce6e21eedbb3d9c5128b34703eb2
jul.neveu@laposte.net <jul.neveu@laposte.net@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-12-07 14:16:47 +0000

    tests Ok

--- Projet/src/test/PromotionTest.java
+++ Projet/src/test/PromotionTest.java
@@ -8,26 +8,20 @@ public class PromotionTest extends TestCase {
 	// test ok
 	
 //	public void testVerifierDatePromotion() throws Exception{
-//		boolean b=true;
-//		assertEquals(b, Promotion.verifierDatePromotion("2015", "12", "24"));
-//		boolean bo=false;
-//		assertEquals(bo, Promotion.verifierDatePromotion("2011", "02", "30"));
-//		assertEquals(bo, Promotion.verifierDatePromotion("2014", "4", "31"));
+//		assertTrue( Promotion.verifierDatePromotion("2015", "12", "24"));
+//		assertFalse( Promotion.verifierDatePromotion("2011", "02", "30"));
+//		assertFalse( Promotion.verifierDatePromotion("2014", "4", "31"));
 //		
 //	}
 
-//	public void testVerifierOrdreDeuxDate() throws Exception{
-//		boolean b=true;
-//		assertEquals(b, Promotion.verifierOrdreDeuxDate("2010", "12", "03", "2010", "12", "05"));
-//		
-//	}
+	// test ok
+	
 	public void testVerifierOrdreDeuxDate2() throws Exception{
-		boolean b=true;
-		boolean bo=false;
-		assertEquals(b, Promotion.verifierOrdreDeuxDate("2010", "12", "03", "2010", "12", "03"));
-		assertEquals(bo, Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2010", "08", "25"));
-		assertEquals(bo, Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2010", "08", "25"));
-		assertEquals(bo, Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2010", "08", "25"));
-		assertEquals(bo, Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2010", "08", "25"));
+		assertTrue(Promotion.verifierOrdreDeuxDate("2010", "12", "03", "2010", "12", "05"));
+		assertTrue(Promotion.verifierOrdreDeuxDate("2010", "12", "03", "2010", "12", "03"));
+		assertFalse(Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2010", "08", "25"));
+		assertTrue( Promotion.verifierOrdreDeuxDate("2010", "08", "27", "2011", "03", "12"));
+		assertFalse( Promotion.verifierOrdreDeuxDate("2011", "08", "27", "2011", "05", "25"));
+		assertFalse( Promotion.verifierOrdreDeuxDate("2011", "08", "27", "2010", "08", "25"));
 	}
 }
