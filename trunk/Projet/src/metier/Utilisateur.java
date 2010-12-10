ebf4c09264d79e3d3d4482d09a433338daacbaf4
Nguyen.Sullivan <Nguyen.Sullivan@084189b5-d0a8-f9c6-a5c9-6fb7bc1859d0>
2010-12-10 12:40:52 +0000

    Javadoc again and again...

--- trunk/Projet/src/metier/Utilisateur.java
+++ trunk/Projet/src/metier/Utilisateur.java
@@ -1,13 +1,81 @@
 package metier;
 
+
+/**
+ * <b>La classe abstraite Utilisateur représente un utilisateur (client ou gérant)</b>
+ * <p>
+ * Tous les clients et le gérant ont en commun les informations suivantes  :
+ * <ul>
+ * <li>Une adresse mail qui sert d'identifiant unique et non modifiable par le client</li>
+ * <li>Une adresse</li>
+ * <li>L'identifiant de la ville de l'utilisateur</li>
+ * <li>Un numéro de téléphone</li>
+ * <li>Un mot de passe généré automatiquement</li>
+ * </ul>
+ * </p>
+ * <p>
+ * 
+ * </p>
+ * 
+ * @see Client, Gerant
+ */
 public abstract class Utilisateur {
 
+	/**
+	 * Le mail de l'utilisateur, non modifiable
+	 * 
+	 * @see Client, Gerant
+	 * 
+	 */
 	protected String mail;
+	
+	/**
+	 * L'adresse de l'utilisateur
+	 * 
+	 * @see Client, Gerant
+	 * 
+	 */
 	protected String adresse;
+	
+	/**
+	 * L'identifiant de la ville de l'utilisateur
+	 * 
+	 * @see Client, Gerant
+	 * 
+	 */
 	protected String idVille;
+	
+	/**
+	 * Le numéro de téléphone de l'utilisateur
+	 * 
+	 * @see Client, Gérant
+	 * 
+	 */
 	protected String telephone;
+	
+	/**
+	 * Le mot de passe de l'utilisateur
+	 * 
+	 * @see Client, Gérant
+	 * @see Utilisateur#genererMdp()
+	 * 
+	 */
 	protected String motDePasse;
 
+	
+	/**
+	 * Génère un mot de passe de 7 caractères
+	 * 
+	 * <p>
+	 * Cette méthode choisit aléatoirement 7 caractères dans la table des caractères
+	 * ASCII (les 48 premiers caractères sont exclus car trop "exotiques")
+	 * 
+	 *
+	 * </p> 
+	 * 
+	 * @return Le mot de passe généré
+	 * @see Client,Gerant
+	 */
 	protected String genererMdp(){
 		String s = "";
 		for(int i=0;i<7;i++){
